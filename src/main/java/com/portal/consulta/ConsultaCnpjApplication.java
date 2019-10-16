7a40ee153fd8962889c28c44bd3e9d4a267407f3
Jeanderson Silva Lopes <jeananimax@gmail.com>
2019-10-16 02:11:03 -0300

    umas paradinhas

--- src/main/java/com/portal/consulta/ConsultaCnpjApplication.java
+++ src/main/java/com/portal/consulta/ConsultaCnpjApplication.java
@@ -1,16 +1,11 @@
 package com.portal.consulta;
 
-import com.portal.consulta.controller.IndexController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@Configuration
 @EnableAutoConfiguration
-@ComponentScan({"com.jeanderson.consulta", "controller"})
 public class ConsultaCnpjApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/portal/consulta/controller/IndexController.java
+++ src/main/java/com/portal/consulta/controller/IndexController.java
@@ -5,10 +5,12 @@
  */
 package com.portal.consulta.controller;
 
+import com.portal.consulta.model.PortalTransparencia;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
@@ -21,6 +23,24 @@ public class IndexController {
     
     @RequestMapping(method = RequestMethod.GET)
     public String index(){
+        System.out.println("rodou");
         return "index";
+    }
+    
+    @RequestMapping(value = "/pesquisa", method = RequestMethod.GET)
+    public ModelAndView pesquisa(@RequestParam("pesquisa") String pesquisa){        
+        PortalTransparencia p = new PortalTransparencia();
+        try {
+            if(p.buscarDados(pesquisa)){
+                ModelAndView mv = new ModelAndView("pesquisa");
+                mv.addObject("dados", p);
+                return mv;
+            }else{
+               return new ModelAndView("index");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return new ModelAndView("index"); 
+        }
     }
 }
